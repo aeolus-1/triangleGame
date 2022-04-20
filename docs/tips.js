ff175f6906a952d4db875f1cb95e88f168ab5eaa
aeolus-1 <102002218+aeolus-1@users.noreply.github.com>
2022-04-21 08:37:41 +1000

    Add files via upload

---
+++ docs/tips.js
@@ -0,0 +1,8 @@
+var tips = [
+    "Apply pressure to the W key to jump",
+    "This is the Gorbnos quest of github games",
+    "You can vote for color themes on the github",
+    
+]
+
+var tip = tips[Math.floor(Math.random()*tips.length)]
