433648445e6fc6ddeded5d7653f636a0b0daff3c
aeolus-1 <102002218+aeolus-1@users.noreply.github.com>
2022-04-21 15:27:24 +1000

    Update tips.js

--- docs/tips.js
+++ docs/tips.js
@@ -76,6 +76,24 @@ var tips = [
 
 
 ];
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+tips = shuffle(tips)
 
 var tip = tips[Math.floor((new Date().getTime()/3000) % tips.length)];
 
