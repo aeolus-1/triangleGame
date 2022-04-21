56329ea01526d7e1373b738c4ac60fd5d55b3719
aeolus-1 <102002218+aeolus-1@users.noreply.github.com>
2022-04-21 13:23:04 +1000

    Update patternDetection.js

--- docs/patternDetection.js
+++ docs/patternDetection.js
@@ -1,70 +1,7 @@
 var patternDetection = {
     keyLog:new Array(),
     patterns:[
-        {
-            label:"konmaiTpMenu",
-            keys:["YXJyb3dsZWZ0","YXJyb3dsZWZ0","YXJyb3dyaWdodA==","YXJyb3dyaWdodA==","YXJyb3d1cA==","YXJyb3dkb3du","YXJyb3d1cA==","YXJyb3dkb3du","Yg==","ZQ=="],
-            callback:function(){
-                var text = "Select Checkpoint\n"
-
-                for (let i = 0; i < Object.keys(checkpoints).length; i++) {
-                    const check = Object.keys(checkpoints)[i];
-                    text += `${i+1}: ${check}\n`
-                }
-
-                var tpNum = parseInt(prompt(text))
-
-
-
-                tp(checkpoints[Object.keys(checkpoints)[tpNum-1]])
-            }
-        },
-        {
-            label:"infinJump",
-            keys:["bg==","aQ==","bQ==","Yg==","YQ==","aA=="],
-            callback:function(){
-                infinJump = true
-            }
-        },
-        {
-            label:"resetShort",
-            keys:["cg==","ZQ==","cw==","ZQ==","dA=="],
-            callback:function(){
-                resetGame()
-            }
-        },
-        {
-            label:"addPlayer",
-            keys:["ag==","bw==","aQ==","bg==","cA==","bA=="],
-            callback:function(){
-                addPlayer()
-            }
-        },
-        {
-            label:"preCheckpoint",
-            keys:["cA==","bw==","bA=="],
-            callback:function(){
-                jumpPreCheckpoint()
-            }
-            
-        },
-        {
-            label:"ghost",
-            keys:["Zw==","aA==","bw==","cw==","dA=="],
-            callback:function(){
-                window.preFilter = {...entitys[0].body.collisionFilter}
-                entitys[0].body.collisionFilter = {
-                    'group': -1,
-                    'category': 2,
-                    'mask': 0,
-                  };
-                  setTimeout(() => {
-                      entitys[0].body.collisionFilter = {...window.preFilter}
-                  }, 500);
-                  
-            }
-            
-        }
+        
     ]
 }
 
