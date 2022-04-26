cfc94f40f395a9343eced6cfc1b8d7b2eb6b685c
Definitely not xl_83 <98920372+LX8369@users.noreply.github.com>
2022-04-26 18:21:33 +1000

    Update multiplayer.js

--- docs/multiplayer.js
+++ docs/multiplayer.js
@@ -1,5 +1,16 @@
-if (confirm("Would you like to join multiplayer? \n \n \n multiplayer made by jake cause im cool")) {
+if (confirm("Would you like to join multiplayer? /n /n multiplayer made by jake cause im cool")) {
     var inactive;
+    var username;
+
+    function askForUser() {
+        var user = prompt("Please enter a username");
+        if (user == null) {
+            askForUser();
+        } else {
+            username = user;
+        }
+
+    }
     const socket = io("https://triangle-game-server.herokuapp.com")
 
     document.addEventListener("keypress", function() {
@@ -8,7 +19,7 @@ if (confirm("Would you like to join multiplayer? \n \n \n multiplayer made by ja
     });
 
     function coords() {
-        socket.emit('coords', { id: socket.id, x: entitys[0].body.position.x, y: entitys[0].body.position.y, velX: entitys[0].body.velocity.x, velY: entitys[0].body.velocity.y, angle: entitys[0].body.angle, angVel: entitys[0].body.angularVelocity });
+        socket.emit('coords', { id: socket.id, x: entitys[0].body.position.x, y: entitys[0].body.position.y, velX: entitys[0].body.velocity.x, velY: entitys[0].body.velocity.y, angle: entitys[0].body.angle, angVel: entitys[0].body.angularVelocity, username: username, scale: entitys[0].scale });
         requestAnimationFrame(coords)
     }
 
@@ -16,25 +27,25 @@ if (confirm("Would you like to join multiplayer? \n \n \n multiplayer made by ja
         inactive = setTimeout(() => {
             socket.emit('inactive');
             alert("disconnected due to inactivity");
-        }, 300000)
+        }, 600000)
     }
 
     socket.on('connect', function() {
-        socket.emit('playerJoin', { id: socket.id, x: entitys[0].body.position.x, y: entitys[0].body.position.y, velX: entitys[0].body.velocity.x, velY: entitys[0].body.velocity.y, angle: entitys[0].body.angle, angVel: entitys[0].body.angularVelocity });
+        socket.emit('playerJoin', { id: socket.id, x: entitys[0].body.position.x, y: entitys[0].body.position.y, velX: entitys[0].body.velocity.x, velY: entitys[0].body.velocity.y, angle: entitys[0].body.angle, angVel: entitys[0].body.angularVelocity, username: username, scale: entitys[0].scale });
     });
 
     socket.on('createPlayer', function(data) {
         if (data.id != socket.id) {
             if (data.id != "undefined") {
                 var newpl = (
-                    new  Multiplayer(v(data.x, data.y), {
+                    new Multiplayer(v(data.x, data.y), {
                         moveLeft: [""],
                         moveRight: [""],
                         jump: [""],
                         duck: [""],
-                    }, data.id)
+                    }, data.id, data.username)
                 )
-                 multiplayers.push(newpl)
+                multiplayers.push(newpl)
                 console.log("adding playyer")
             }
         }
@@ -46,12 +57,14 @@ if (confirm("Would you like to join multiplayer? \n \n \n multiplayer made by ja
 
     socket.on('updatePlayers', function(data) {
         Object.keys(data).every(function(key) {
-            for (var i = 0; i <  multiplayers.length; i++) {
-                if (key ==  multiplayers[i].multiId) {
-                    Matter.Body.set( multiplayers[i].body, "position", v(data[key].x, data[key].y));
-                    Matter.Body.set( multiplayers[i].body, "angle", data[key].angle);
-                    Matter.Body.set( multiplayers[i].body, "velocity", v(data[key].velX, data[key].velY));
-                    Matter.Body.set( multiplayers[i].body, "angularVelocity", data[key].angVel);
+            for (var i = 0; i < multiplayers.length; i++) {
+                if (key == multiplayers[i].multiId) {
+                    Matter.Body.set(multiplayers[i].body, "position", v(data[key].x, data[key].y));
+                    Matter.Body.set(multiplayers[i].body, "angle", data[key].angle);
+                    Matter.Body.set(multiplayers[i].body, "velocity", v(data[key].velX, data[key].velY));
+                    Matter.Body.set(multiplayers[i].body, "angularVelocity", data[key].angVel);
+                    multiplayers[i].scale = data[key].scale;
+                    render.context.fillText(data[key].username, data[key].x, data[key].y)
                 }
             }
             return true
@@ -68,9 +81,9 @@ if (confirm("Would you like to join multiplayer? \n \n \n multiplayer made by ja
                             moveRight: [""],
                             jump: [""],
                             duck: [""],
-                        }, key)
+                        }, key, data[key].username)
                     )
-                     multiplayers.push(newpl)
+                    multiplayers.push(newpl)
                 }
             }
             return true
@@ -78,12 +91,13 @@ if (confirm("Would you like to join multiplayer? \n \n \n multiplayer made by ja
     })
 
     socket.on('removePlayer', function(data) {
-        for (var i = 0; i <  multiplayers.length; i++) {
-            if (data ==  multiplayers[i].multiId) {
-                Matter.Composite.remove(engine.world,  multiplayers[i].body)
-                 multiplayers.splice(i, 1);
+        for (var i = 0; i < multiplayers.length; i++) {
+            if (data == multiplayers[i].multiId) {
+                Matter.Composite.remove(engine.world, multiplayers[i].body)
+                multiplayers.splice(i, 1);
             }
         }
     })
+    askForUser()
     startTimer()
 }
