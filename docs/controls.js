293239c96a2a88e07373da7523e10dfdf7dfb4ea
aeolus-1 <102002218+aeolus-1@users.noreply.github.com>
2022-04-28 13:29:02 +1000

    Update controls.js

--- docs/controls.js
+++ docs/controls.js
@@ -4,15 +4,52 @@ var keys = {},
 var startTime = false,
         showMap = false
 
+var textMsg = "",
+    typing = false
+
+var chat = []
+
 document.addEventListener("keydown", function(e){
     keys[(e.key).toLowerCase()]=true;
-    if (startTime == false) {
-        startTime = new Date().getTime()
+
+    var k = (e.key).toLowerCase()
+    if (k == "enter") {
+        typing = false
+        console.log(textMsg)
+        var length = render.context.measureText(textMsg).width
+        chat.push({
+            text:textMsg,
+            pos:v(entitys[0].body.position.x-(length/2), entitys[0].body.position.y-50),
+            time:100,
+        })
+        textMsg = ""
     }
-    patternDetection.logKey((e.key).toLowerCase())
-    
-    if ((e.key).toLowerCase() == "y") {
-            showMap = !showMap
+
+    if (!typing) {
+
+        if (startTime == false) {
+            startTime = new Date().getTime()
         }
+        patternDetection.logKey((e.key).toLowerCase())
+        
+        if ((e.key).toLowerCase() == "y") {
+                showMap = !showMap
+            }
+        } else {
+            if (k != "backspace") {
+                if ((k.length == 1) )textMsg = textMsg + k
+            } else {
+                var split = (textMsg).split("")
+                split.splice(split.length-1, 1)
+                textMsg = split.join("")
+            }
+
+        }
+
+        if (k == "p") {
+            typing = true
+        }
+
+        
 })
 document.addEventListener("keyup", function(e){keys[(e.key).toLowerCase()]=undefined;delete keys[(e.key).toLowerCase()]})
