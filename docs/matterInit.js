5f819f743a45877d701d5529e15fe3cd5592b480
Definitely not xl_83 <98920372+LX8369@users.noreply.github.com>
2022-04-28 18:27:57 +1000

    Update matterInit.js

--- docs/matterInit.js
+++ docs/matterInit.js
@@ -290,14 +290,14 @@ Matter.Events.on(render, "afterRender", function() {
     render.context.fillText(`FPS: ${fps}`, 10, 150)
     if (multiplayers.length > 0) render.context.fillText(`players: ${multiplayers.length+1}`, 10, 195)
        var msgAmount = 0
-        var msgDist = 350
+        var msgDist = 500
     
     
     for (var i = 0; i < chatMsg.length; i++) {
         render.context.fillText(`${chatMsg[i].username}: ${chatMsg[i].message}`, 10, msgDist)
-        msgDist += 50
+        msgDist += 30
         msgAmount++
-        if (msgAmount == 5) {
+        if (msgAmount == 10) {
         chatMsg.shift()
         msgAmount -= 1
 }
