d2a12032f4e31337c9e3957118b40b06578045b2
Definitely not xl_83 <98920372+LX8369@users.noreply.github.com>
2022-05-03 19:13:41 +1000

    dfdfd

--- docs/matterInit.js
+++ docs/matterInit.js
@@ -308,7 +308,7 @@ Matter.Events.on(render, "afterRender", function() {
     render.context.fillText(`${Math.round(Matter.Common.clamp((-entitys[0].body.position.y+10)/100, 0, Infinity))+2}m`, 10, 90)
     render.context.fillText(`Speed: ${Math.abs(Math.round(entitys[0].body.velocity.x * 100) / 100)}`, 10, 120)
     render.context.fillText(`FPS: ${fps}`, 10, 150)
-    render.context.fillText(`Ping: ${clientPing}`, 10, 180)
+    render.context.fillText(`Ping: ${clientPing}ms`, 10, 180)
     if (multiplayers.length > 0) render.context.fillText(`players: ${multiplayers.length+1}`, 10, 210)
 
 
