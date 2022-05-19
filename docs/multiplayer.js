528feefe5baaaff364edd35b5201d2aed56be983
Definitely not xl_83 <98920372+LX8369@users.noreply.github.com>
2022-05-20 08:59:31 +1000

    Update multiplayer.js

--- docs/multiplayer.js
+++ docs/multiplayer.js
@@ -102,7 +102,7 @@ if (confirm("Would you like to join multiplayer? \n \n \n multiplayer made by ja
 
 
     socket.on('connect', function() {
-        socket.emit('playerJoin', { id: socket.id, x: entitys[0].body.position.x, y: entitys[0].body.position.y, velX: entitys[0].body.velocity.x, velY: entitys[0].body.velocity.y, angle: entitys[0].body.angle, angVel: entitys[0].body.angularVelocity, username: username, scale: getPlayerScale(entitys[0]), chat: chat });
+        socket.emit('playerJoin', { id: socket.id, x: entitys[0].body.position.x, y: entitys[0].body.position.y, velX: entitys[0].body.velocity.x, velY: entitys[0].body.velocity.y, angle: entitys[0].body.angle, angVel: entitys[0].body.angularVelocity, username: username, scale: getPlayerScale(entitys[0]), chat: chat, browserId: entitys[0].browserId });
 
         socket.emit("sendMessage", { message: { time: 100 }, username: username, type: "join" })
     });
