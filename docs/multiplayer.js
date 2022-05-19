ccf8cd2c082d587eb2c85cc880bee1d2c7f42ac8
Definitely not xl_83 <98920372+LX8369@users.noreply.github.com>
2022-05-20 09:10:20 +1000

    Update multiplayer.js

--- docs/multiplayer.js
+++ docs/multiplayer.js
@@ -88,7 +88,7 @@ if (confirm("Would you like to join multiplayer? \n \n \n multiplayer made by ja
     });
 
     function coords() {
-        socket.emit('coords', { id: socket.id, x: entitys[0].body.position.x, y: entitys[0].body.position.y, velX: entitys[0].body.velocity.x, velY: entitys[0].body.velocity.y, angle: entitys[0].body.angle, angVel: entitys[0].body.angularVelocity, username: username, scale: getPlayerScale(entitys[0]), chat: chat });
+        socket.emit('coords', { id: socket.id, x: entitys[0].body.position.x, y: entitys[0].body.position.y, velX: entitys[0].body.velocity.x, velY: entitys[0].body.velocity.y, angle: entitys[0].body.angle, angVel: entitys[0].body.angularVelocity, username: username, scale: getPlayerScale(entitys[0]), chat: chat, browserId: entitys[0].browserId});
         requestAnimationFrame(coords)
     }
 
@@ -116,7 +116,7 @@ if (confirm("Would you like to join multiplayer? \n \n \n multiplayer made by ja
                         moveRight: [""],
                         jump: [""],
                         duck: [""],
-                    }, data.id, data.username)
+                    }, data.id, data.username, data.browserId)
                 )
                 multiplayers.push(newpl)
 
@@ -172,7 +172,7 @@ if (confirm("Would you like to join multiplayer? \n \n \n multiplayer made by ja
                             moveRight: [""],
                             jump: [""],
                             duck: [""],
-                        }, key, data[key].username)
+                        }, key, data[key].username, data[key].browserId)
                     )
                     multiplayers.push(newpl)
 
