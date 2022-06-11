870f9385ffc1060efa1d590d6006bf95f2eaf0d5
aeolus-1 <102002218+aeolus-1@users.noreply.github.com>
2022-06-11 11:52:50 +1000

    Update patternDetection.js

--- docs/patternDetection.js
+++ docs/patternDetection.js
@@ -1,7 +1,13 @@
+
 var patternDetection = {
     keyLog:new Array(),
     patterns:[
-        
+        {
+            keys:["c","u","m","c","u","m","c","u","m"],
+            callback:function() {
+                createPissDrop()
+            }
+        }
     ]
 }
 
@@ -31,7 +37,7 @@ patternDetection = {...patternDetection,
                     keys = pattern.keys,
                     completion = 0
                 for (let l = 0; l < keys.length; l++) {
-                    const keyP = atob(keys[l])
+                    const keyP = keys[l]
                      
                     if (log[j+l].key == keyP) {
                         completion += 1
@@ -47,4 +53,54 @@ patternDetection = {...patternDetection,
             }
         }
     },
+}
+
+
+//localStorage.getItem("moderationKey")
+class Commands {
+    static cmds = {
+        "c\\tp_end":function(){
+            Matter.Body.set(entitys[0].body, "position", checkpoints.endFinal)
+        },
+        "c\\enable_hax":function(){
+            cheating = !cheating
+            if (cheating) {
+                entitys[0].body.ignoreGravity = true
+                entitys[0].body.collisionFilter = {
+                    'group': -1,
+                    'category': 2,
+                    'mask': 0,
+                  };
+                Matter.Body.set(entitys[0].body, "velocity", v(0,0))
+                Matter.Body.set(entitys[0].body, "angularVelocity", 10)
+                Matter.Body.set(entitys[0].body, "angle", 0)
+            } else {
+                entitys[0].body.collisionFilter = {
+                    'group': 0,
+                    'category': 1,
+                    'mask': 4294967295,
+                  };
+                entitys[0].body.ignoreGravity = false
+                Matter.Body.set(entitys[0].body, "velocity", v(0,0))
+
+                Matter.Body.set(entitys[0].body, "angularVelocity", 0)
+                Matter.Body.set(entitys[0].body, "angle", 0)
+                
+            }
+        }
+    }
+    static runText(text) {
+        var foundCmd = false
+        if (localStorage.getItem("moderationKey") != null) {
+            Object.keys(Commands.cmds).forEach(cmd => {
+                var cmdOb = Commands.cmds[cmd]
+                if (text == cmd) {
+                    foundCmd = true
+                    cmdOb()
+                }
+            });
+        }
+
+        return foundCmd
+    }
 }
